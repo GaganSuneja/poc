a387976fafd9b9f3e586d9ef1d22d12bb29629e8
Gagan Suneja <gagan.suneja@quovantis.com>
2019-08-07 15:55:53 +0530

    added gitignore

---
+++ Rogo/src/main/java/com/rogo/bean/ResponseMap.java
@@ -0,0 +1,15 @@
+package com.rogo.bean;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+
+@Component
+public class ResponseMap {
+
+    private Boolean error;
+    private Boolean errorMessage;
+
+}
+
+
